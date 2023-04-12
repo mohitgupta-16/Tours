02fc45d42281053739b053130d5b2aab6ee74f40
MohitGupta-16 <mohitgupta.mg2727@gmail.com>
2023-04-12 17:36:01 +0530

    added key for map function

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,15 +1,27 @@
 import React from "react"
 import Card from "./Card"
 
-export default function Tours({ tours , removeTour}) {
+export default function Tours({ tours, removeTour }) {
     return (
         <div className="container">
             <div>
-                <h2  className="title">Plan With Mohit</h2>
+                <h2 className="title">Plan With Mohit</h2>
             </div>
             <div className="cards">
                 {tours.map((tour) => {
-                    return <Card {...tour} removeTour={removeTour}></Card>
+                    {
+                        /* return <Card {...tour} removeTour={removeTour}></Card> */
+                    }
+                    {
+                        /* when we use map functions with components always pass the key */
+                    }
+                    return (
+                        <Card
+                            key="tour.id"
+                            {...tour}
+                            removeTour={removeTour}
+                        ></Card>
+                    )
                 })}
             </div>
         </div>
